8ac51dd4571de2bca165a6ebaa3afb51e182abd4
ZoNampoina <zonampari@gmail.com>
2026-09-24 18:59:18 +0300

    chore: bump DI'ART to 2.9.10

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ import { supabase, syncAll, signIn, signOut, signUp, getCloudStats, pullCloudToL
 import { parseChordPro } from './recueils'
 import { fetchTononkiraReference, searchTononkira, searchExternalRecueil, importExternalRecueil, type TononkiraSearchResult, type ExternalRecueilSource, type ExternalRecueilResult } from './catalog'
 
-const APP_VERSION='2.9.9'
+const APP_VERSION='2.9.10'
 
 const navItems = [
   ['dashboard','Accueil',Home], ['library','Bibliothèque',Library], ['artists','Artistes',UsersRound],
